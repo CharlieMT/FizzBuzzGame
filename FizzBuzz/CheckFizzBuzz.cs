b476917e1d4c5bd909d684e8a559bae326648f75
Charlie_MT <pavvelzawadzki@gmail.com>
2022-06-06 17:22:09 +0200

    running fizz buzz app code

---
+++ FizzBuzz/CheckFizzBuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class CheckFizzBuzz
+    {
+        string fizzBuzz = "";
+        internal string FizzBuzzCHeck(int number)
+        {
+
+            if (number % 3 == 0)
+                fizzBuzz = "Fizz";
+            if (number % 5 == 0)
+                fizzBuzz = fizzBuzz + "Buzz";
+
+            if (String.IsNullOrWhiteSpace(fizzBuzz))
+                fizzBuzz = number.ToString();
+
+            return fizzBuzz;
+
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello in FizzBuzz app \nWe will check if your number is divided by 3 or 5 and return Fizz, Buzz or FizzBuzz message ;)");
+
+            while (true)
+            {
+
+                CheckFizzBuzz checkFizzBuzz = new CheckFizzBuzz();
+
+                var numberCheck = Program.InputCheck();
+
+                Console.WriteLine(checkFizzBuzz.FizzBuzzCHeck(numberCheck));
+
+                if (ExitMenu())
+                    break;
+            }
+        }
+
+        private static int InputCheck()
+        {
+            while (true)
+            {
+                Console.WriteLine("Please provide your number");
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Wrong value provided :( ");
+                    continue;
+                }
+
+                return number;
+            }
+        }
+
+        private static bool ExitMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("Type X to Exit or C to Continue");
+                var answer = Console.ReadLine().ToUpper();
+
+                if (answer.Equals("X"))
+                    return true;
+
+                if (answer.Equals("C"))
+                    return false;
+            }
+        }
+    }
+}
